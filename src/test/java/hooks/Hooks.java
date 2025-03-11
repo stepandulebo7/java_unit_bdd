3bc4e842152760544c00ba887bbde774f0cc73d6
stepandulebo7 <stepandulebo7@gmail.com>
2025-03-11 22:43:18 +0300

    Commit

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,5 +1,27 @@
 package hooks;
 
+import aquality.selenium.browser.AqualityServices;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import utils.EnvDataReader;
+
+
+import static aquality.selenium.browser.AqualityServices.getBrowser;
+
 public class Hooks {
-    // todo: add hooks for pre and post conditions
+    @Before()
+    @Given("The user is on the main page")
+      public void setup() {
+        getBrowser().maximize();
+        getBrowser().goTo(EnvDataReader.getEnvData().getHost());
+    }
+
+    @After()
+    public void teardown() {
+        if (AqualityServices.isBrowserStarted()) {
+            //  step("Quiting the browser");
+         //   getBrowser().quit();
+        }
+    }
 }
--- src/test/java/stepdefinitions/DataTableSteps.java
+++
@@ -1,5 +0,0 @@
-package stepdefinitions;
-
-public class DataTableSteps {
-    // todo: implement
-}
